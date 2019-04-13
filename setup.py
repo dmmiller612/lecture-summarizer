f7a24983ce566a2e25b22c2c484ca033f759112a
dmmiller612 <dmmiller612@gmail.com>
2019-04-13 15:37:43 -0400

    Added requests

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='lecture-summarizer',
     version='0.0.1',
+    install_requires=[
+        'requests',
+    ],
     entry_points={
         'console_scripts': [
             'lecture-summarizer=lecture_summarizer:run'
